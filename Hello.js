a93ae4fdeeda6338d38467161b62a05c7c74a32f
PP-ige <mynewspamwhoop@gmail.com>
2025-01-20 22:40:03 -0500

    fr

--- Hello.js
+++ Hello.js
@@ -1,75 +1,26 @@
-//Based on the Scroller function from @sallar
-var $content = $('header .content')
-  , $blur    = $('header .overlay')
-  , wHeight  = $(window).height();
+var mysql = require('mysql');
 
-$(window).on('resize', function(){
-  wHeight = $(window).height();
+var connection = mysql.createConnection({
+    host: "localhost",
+    database: "test",
+    user: "root",
+    password: "j03_m@m@"
 });
 
-window.requestAnimFrame = (function()
-{
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-function Scroller()
-{
-  this.latestKnownScrollY = 0;
-  this.ticking            = false;
-}
-
-Scroller.prototype = {
- 
-  init: function() {
-    window.addEventListener('scroll', this.onScroll.bind(this), false);
-    $blur.css('background-image',$('header:first-of-type').css('background-image'));
-  },
-
-
-  onScroll: function() {
-    this.latestKnownScrollY = window.scrollY;
-    this.requestTick();
-  },
-
-  
-  requestTick: function() {
-    if( !this.ticking ) {
-      window.requestAnimFrame(this.update.bind(this));
+connection.connect(function(err){
+    if(err){
+    console.error('Error connecting:' + err.stack);
+    return; 
     }
-    this.ticking = true;
-  },
 
-  update: function() {
-    var currentScrollY = this.latestKnownScrollY;
-    this.ticking       = false;
-    
-    
-    var slowScroll = currentScrollY / 2
-      , blurScroll = currentScrollY * 2
-      , opaScroll = 1.4 - currentScrollY / 400;
-   if(currentScrollY > wHeight)
-     $('nav').css('position','fixed');
-   else
-     $('nav').css('position','absolute');
-    
-    $content.css({
-      'transform'         : 'translateY(' + slowScroll + 'px)',
-      '-moz-transform'    : 'translateY(' + slowScroll + 'px)',
-      '-webkit-transform' : 'translateY(' + slowScroll + 'px)',
-      'opacity' : opaScroll
+    console.log('Connected as id' + connection.threadId);
+});
+
+connection.query('select * from testtable', function (error, results, fields){
+    if(error)
+        throw error;
+
+    results.forEach(result => {
+        console.log(result)
     });
-    
-    $blur.css({
-      'opacity' : blurScroll / wHeight
-    });
-  }
-};
-
-
-var scroller = new Scroller();  
-scroller.init();
+});
